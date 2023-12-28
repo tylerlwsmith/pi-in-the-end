41c76dd88bd7a9125aa4980bde7247707f141d33
Tyler Smith <tylerlwsmith@gmail.com>
2023-12-28 13:42:16 -0800

    Catch KeyboardInterrupt for more graceful shutdown

--- midi_listener.py
+++ midi_listener.py
@@ -20,21 +20,25 @@ melody = [to_number(note) for note in melody_note_names]
 entered_notes = []
 
 input_port = mido.open_input()
-print("Listening for input...")
+print("Listening for input ...")
 
-for msg in input_port:
-    if msg.type == "note_on" and msg.velocity > 0:
-        # Append, disregarding octaves. C is 0, C# is 1, etc.
-        entered_notes.append(msg.note % 12)
+try:
+    for msg in input_port:
+        if msg.type == "note_on" and msg.velocity > 0:
+            # Append, disregarding octaves. C is 0, C# is 1, etc.
+            entered_notes.append(msg.note % 12)
 
-        # Only keep around enough notes to see if it matches.
-        while len(entered_notes) > len(melody):
-            entered_notes.pop(0)
+            # Only keep around enough notes to see if it matches.
+            while len(entered_notes) > len(melody):
+                entered_notes.pop(0)
 
-        if entered_notes == melody:
-            # Reset notes to prevent half matching weirdness.
-            entered_notes.clear()
-            print("🎶 I tried so hard, and got so far 🎶")
+            if entered_notes == melody:
+                # Reset notes to prevent half matching weirdness.
+                entered_notes.clear()
+                print("🎶 I tried so hard, and got so far 🎶")
 
-            if os.path.isfile(audio_file_path):
-                playsound(audio_file_path, block=True)
+                if os.path.isfile(audio_file_path):
+                    playsound(audio_file_path, block=True)
+
+except KeyboardInterrupt:
+    print("\nShutting down ...")
